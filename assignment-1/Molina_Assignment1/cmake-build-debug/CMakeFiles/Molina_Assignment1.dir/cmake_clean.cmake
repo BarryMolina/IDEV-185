34098e7400cb6245e91a2a76cbd1c7888017fbc5
Barry Molina <bazzaboy@gmail.com>
2021-01-23 10:25:46 -0600

    start assignment 1

---
+++ assignment-1/Molina_Assignment1/cmake-build-debug/CMakeFiles/Molina_Assignment1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Molina_Assignment1.dir/main.cpp.o"
+  "Molina_Assignment1"
+  "Molina_Assignment1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Molina_Assignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ assignment-1/Molina_Assignment1/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * Barry Molina
+ * ITDEV-185-900
+ * Assignment 1
+ */
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
