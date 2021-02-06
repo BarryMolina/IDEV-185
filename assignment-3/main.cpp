fe0c1b334265adb6fabd3e2f863d645594623573
Barry Molina <bazzaboy@gmail.com>
2021-02-06 13:02:49 -0600

    completed assignment 3

--- assignment-3/main.cpp
+++ assignment-3/main.cpp
@@ -1,17 +1,34 @@
+/*
+ * Barry Molina
+ * ITDEV-185-900
+ * Assignment 3
+ */
+
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <iomanip>
 using namespace std;
 
 void inputData(int &ordered, int &inStock, double &extra);
-void displayOrder();
+void displayOrder(int ordered, int inStock, double costPerSpool, double shippingRate = 10.00);
+
+const double SPOOL_COST = 100.00;
 
 int main()
 {
     int spoolsOrdered, spoolsInStock;
     double extraShipping;
 
-//    inputData(spoolsOrdered, spoolsInStock, extraShipping);
-    displayOrder();
+    inputData(spoolsOrdered, spoolsInStock, extraShipping);
+    if (extraShipping > 0)
+    {
+        displayOrder(spoolsOrdered, spoolsInStock, SPOOL_COST, 10.00 + extraShipping );
+    }
+    else
+    {
+        displayOrder(spoolsOrdered, spoolsInStock, SPOOL_COST);
+    }
 
     return 0;
 }
@@ -27,17 +44,40 @@ void inputData(int &ordered, int &inStock, double &extra)
     cin >> extra;
 }
 
-void displayOrder()
+void displayOrder(int ordered, int inStock, double costPerSpool, double shippingRate)
 {
-    cout << left;
+    int maxWidth, thisShipment, backOrdered = 0;
+    double spoolCharges, shippingCharges, total;
+    stringstream stream;
+
+    if (ordered > inStock)
+    {
+        thisShipment = inStock;
+        backOrdered = ordered - inStock;
+    }
+    else
+    {
+        thisShipment = ordered;
+    }
+
+    spoolCharges = thisShipment * costPerSpool;
+    shippingCharges = thisShipment * shippingRate;
+    total = spoolCharges + shippingCharges;
+
+    // get max width of numeric portion of report
+    stream << fixed << setprecision(2) << total;
+    maxWidth = stream.str().length();
+
     cout << "      *** Order Summary ***" << endl << endl;
-    cout << setw(29) << "Spools ordered:" << 500 << endl;
-    cout << setw(29) << "Spools in this shipment:" << 500 << endl;
+    cout << left << setw(24) << "Spools ordered:" << right << setw(maxWidth) << ordered << endl;
+    cout << left << setw(24) << "Spools in this shipment:" << right << setw(maxWidth) << thisShipment << endl;
+    if (backOrdered > 0)
+        cout << left << setw(24) << "Spools back ordered:" << right << setw(maxWidth) << backOrdered << endl;
 
     cout << fixed << setprecision(2);
     cout << "\n   Charges for this shipment" << endl;
     cout << "   -------------------------" << endl;
-    cout << setw(22) << "Spool charges:" << "$ " << 50000.000 << endl;
-    cout << setw(22) << "Shipping charges:" << "$ " << 5000.0000 << endl;
-    cout << setw(22) << "Total this shipment:" << "$ " << 55000.0000 << endl;
+    cout << left << setw(22) << "Spool charges:" << "$ " << right << setw(maxWidth) << spoolCharges << endl;
+    cout << left << setw(22) << "Shipping charges:" << "$ " << right << setw(maxWidth) << shippingCharges << endl;
+    cout << left << setw(22) << "Total this shipment:" << "$ " << right << setw(maxWidth) << total << endl;
 }
