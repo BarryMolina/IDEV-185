49c0277d40a158786e74492a1351c482caca1ba7
Barry Molina <bazzaboy@gmail.com>
2021-03-02 12:38:18 -0600

    Assignment 7

---
+++ Molina_Assignment7/Coin.h
@@ -0,0 +1,49 @@
+//
+// Created by Barry Molina on 3/2/21.
+//
+
+#ifndef MOLINA_ASSIGNMENT7_COIN_H
+#define MOLINA_ASSIGNMENT7_COIN_H
+
+#include <string>
+using namespace std;
+
+class Coin {
+private:
+    static double balance;
+    string sideUp;
+    bool heads;
+    double value;
+
+public:
+    Coin(double);
+    bool getHeads();
+    double getValue();
+    string getSideUp();
+    static double getBalance();
+    static void resetBalance();
+    void toss();
+    void addBalance();
+};
+
+// Derived Class Definitions
+// Each derived class calls the parameterized constructor of the Coin superclass with the appropriate value.
+class Quarter : public Coin
+{
+public:
+    Quarter() : Coin(0.25) {}
+};
+
+class Dime : public Coin
+{
+public:
+    Dime() : Coin(0.10) {}
+};
+
+class Nickel : public Coin
+{
+public:
+    Nickel() : Coin(0.05) {}
+};
+
+#endif //MOLINA_ASSIGNMENT7_COIN_H
---
+++ Molina_Assignment7/cmake-build-debug/CMakeFiles/Molina_Assignment7.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Molina_Assignment7.dir/Coin.cpp.o"
+  "CMakeFiles/Molina_Assignment7.dir/main.cpp.o"
+  "Molina_Assignment7"
+  "Molina_Assignment7.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Molina_Assignment7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
